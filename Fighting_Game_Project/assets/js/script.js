28d121917e83915d90a16b2440eb3a4aec635b5b
Uendel <uendel.profissional@gmail.com>
2024-07-22 22:33:10 -0400

    stage class added, start, update and attack functions generated. possible updates may occur

--- Fighting_Game_Project/assets/js/script.js
+++ Fighting_Game_Project/assets/js/script.js
@@ -1,3 +1,7 @@
 let char = new Knight('UendeL');
 let monster = new LittleMonster();
 
+const stage = new Stage(char, monster, document.querySelector('#char'), document.querySelector('#monster'));
+
+stage.start();
+
